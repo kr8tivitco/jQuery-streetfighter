e9a9471f03c0c67426a112a656fbe0f34058491a
kr8tivitco <knmckinney@Kristons-MacBook-Pro.local>
2014-06-13 05:28:19 -0400

    Adding X Action

---
+++ js/app.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+  $('.ryu').mouseenter(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  })
+  .mousedown(function() {
+    playHadouken();    
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').finish().show()
+    .animate(
+      {'left': '300px'},
+      500,
+      function() {
+        $(this).hide();
+        $(this).css('left', '-212px');
+      }
+    );
+  })
+  .mouseup(function() {
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
+  });
+//
+  $(document).keydown(function (event) {
+
+    if(event.which == 88) {
+
+        $('.ryu-ready').hide();
+        $('.ryu-cool').show();
+    }
+  })
+   $(document).keyup(function (event) {
+
+    if(event.which == 88) {
+
+      //alert('You pressed X!');
+        $('.ryu-cool').hide();
+        $('.ryu-ready').show();
+
+    }    
+  })
+});
+  
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
+
+
+
+/*$(document).keydown(function (event) {
+  //if(x.which == 88 && $('.ryu-ready').is(':visible')) 
+
+	if(event.which == 88) {
+        $('.ryu-ready').hide();
+        $('.ryu-cool').show();
+    }
+    
+$(document).keyup(function (event) {
+  if(event.which == 88) {
+        $('.ryu-cool').hide();
+        $('.ryu-ready').show();
+    }
+})*/
+//
+//
+//
+//
+
+  
